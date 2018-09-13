b7f3acafaa19fb904f23a400a626017d39bdff3c
Pitz Michaël <michaeljpitz@gmail.com>
2018-09-13 15:18:41 +0200

    :ok_hand: number2 yaml router

--- src/Controller/HasardController.php
+++ src/Controller/HasardController.php
@@ -5,14 +5,22 @@
 namespace App\Controller;
 
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
-class HasardController {
+class HasardController extends AbstractController {
 
     public function number() {
         $number = random_int(0, 100);
         return new Response(
                 '<html><body>Nombre au hasard: ' . $number . '</body></html>'
         );
+    }
+
+    public function number2($n) {
+
+        return $this->render('number2.html.twig', [
+                    'number' => $n,
+        ]);
     }
 
 }
